1c6f8c75edb388911a5fa205168e05cf68c5317a
lubaba3 <lubabasalah520@gmail.com>
2023-10-31 18:40:22 -0700

    libmy

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,6 +1,10 @@
+/**
+ * main funtion description: main - entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
 int main(void)
-{
-_puts("\"At the end of the day,
+{ _puts("\"At the end of the day,
 my goal was to be the best hacker\"\n\t
 - Kevin Mitnick");
 return (0);
