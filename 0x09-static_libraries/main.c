07bc11f9533b05f95f16267bdb237f4e0448d7ad
lubaba3 <lubabasalah520@gmail.com>
2023-11-01 18:35:18 -0700

    libmy

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,12 @@
+/**
+ * main description: main point - entry point funtion - main funtion
+ * description of main funtion: main point - Entry point
+ * Description: 'alx task statstic lib'
+ * Return: Always 0 (Success)
+ */
 #include "main.h"
-
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t-Kevin Mitnick");
+return (0);
 }
